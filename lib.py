59afc37961e70be7617d2ec6403065fe67f216b9
student <student@local.ru>
2019-02-10 09:20:14 +0300

    Complex if logic

---
+++ lib.py
@@ -0,0 +1,30 @@
+def is_creditable(age, salary):
+    # позитивный тест
+    """
+    >>> is_creditable(30, 40000)
+    True
+
+    >>> is_creditable(20, 40000)
+    False
+
+    >>> is_creditable(70, 40000)
+    False
+
+    >>> is_creditable(30, 10000)
+    False
+    """
+    min_age = 21
+    max_age = 60
+    min_salary = 30000
+
+    if age >= min_age:
+        if age < max_age:
+            if salary >= min_salary:
+                return True
+            else:
+                return False
+        else:
+            return False
+    else:
+        return False
+
