8ec5915dfe4d4596a180a49769f2507d1ec08b20
student <student@local.ru>
2019-02-10 09:31:13 +0300

    Refactoring: интервальная проверка

--- lib.py
+++ lib.py
@@ -17,7 +17,7 @@ def is_creditable(age, salary):
     max_age = 60
     min_salary = 30000
 
-    if age >= min_age and age < max_age and salary >= min_salary:
+    if min_age <= age < max_age and salary >= min_salary:
         return True
     else:
         return False
