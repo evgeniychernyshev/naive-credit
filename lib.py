21065b06c2293dd898093e9305538215cfed7cc0
student <student@local.ru>
2019-02-10 09:40:55 +0300

    Refactoring: early exit

--- lib.py
+++ lib.py
@@ -17,8 +17,11 @@ def is_creditable(age, salary):
     max_age = 60
     min_salary = 30000
 
-    if min_age <= age < max_age and salary >= min_salary:
-        return True
-    else:
+    if age < min_age:
+        return False
+    if age > max_age:
+        return False
+    if salary < min_salary:
         return False
 
+    return True
